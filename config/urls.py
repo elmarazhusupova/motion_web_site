974516349c652b740c5d2a0cbac98f8cfcdd2d18
elmarazhusupova <zhusupovaelmara80@gmail.com>
2023-01-29 15:31:34 +0600

    change password

--- config/urls.py
+++ config/urls.py
@@ -10,5 +10,4 @@ urlpatterns = [
     path('api/logout/', knox_views.LogoutView.as_view(), name='logout'),
     # path('api/logoutall/', knox_views.LogoutAllView.as_view(), name='logoutall'),
     path('api/change-password/', ChangePasswordView.as_view(), name='change-password'),
-    # path('api/password_reset/', include('django_rest_passwordreset.urls', namespace='password_reset')),
 ]
